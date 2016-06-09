527b601cf254dcbee8193d4165b7faea06228e47
MalvinJazz <malvinjazz@live.com>
2016-06-08 22:28:26 -0600

    App Comodin creada, modelos registrados en Admin.

--- src/Usuario/models.py
+++ src/Usuario/models.py
@@ -32,21 +32,6 @@ class Empleado(models.Model):
     def __unicode__(self):
         return str(nombre) + str(apellidos)
 
-class Comodin(models.Model):
-    id = models.AutoField(primary_key = True)
-    nombre = models.CharField(max_length = 45, blank = False, null = True)
-    empresa = models.CharField(max_length = 45, blank = False, null = True)
-    direccion = models.CharField(max_length = 45, blank = False, null = True)
-    telefono = models.CharField(max_length = 8)
-    nit = models.CharField(max_length = 20, blank = False, null = True)
-    tipo = models.BooleanField(default = False)
-    bloqueado = models.BooleanField(default = False)
-    saldo = modesl.DecimalField(decimal_places = 2, blank = False, null = True)
-
-    def __unicode__(self):
-        return str(self.nombre) + "--" + str(self.empresa)
-
-
 class UsuarioManager(BaseUserManager):
 
     def create_user(self, username, Empleado_id, password=None):
