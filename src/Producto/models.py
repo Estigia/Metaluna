5c88adaa42ec616d719d90bd4db34094b17ad8db
Chofo2003 <chofo_03@hotmail.com>
2016-06-08 20:24:17 -0600

    Agregado el unicode a los modelos

--- src/Producto/models.py
+++ src/Producto/models.py
@@ -7,6 +7,9 @@ class Lote(models.Model):
     precio_compra = models.FloatField(blank = True, null=True)
     Producto_id = ForeignKey('Producto')
 
+    def __unicode__(self):
+        return str(self.id)
+
 class Producto(models.Model):
     id = models.AutoField(primary_key = True)
     descripcion = models.CharField(max_length = 60)
@@ -17,22 +20,40 @@ class Producto(models.Model):
     Forma_id = ForeignKey('Forma')
     Marca_id = ForeignKey('Comodin.Marca')
 
+    def __unicode__(self):
+        return self.descripcion
+
 class Tipo_Producto(models.Model):
     id = models.AutoField(primary_key = True)
     tipo = models.CharField(max_length = 45, blank = True, null = True)
+
+    def __unicode__(self):
+        return self.tipo
 
 class Material(models.Model):
     id = models.AutoField(primary_key = True)
     material = models.CharField(max_length = 45, blank = True, null = True)
 
+    def __unicode__(self):
+        return self.material
+
 class Longitud(models.Model):
     id = models.AutoField(primary_key = True)
     longitud = models.IntegerField(blank = True, null = True)
+
+    def __unicode__(self):
+        return str(self.longitud)
 
 class Calibre(models.Model):
     id = models.AutoField(primary_key = True)
     calibre = models.FloatField(blank = True, null = True)
 
+    def __unicode__(self):
+        return str(self.calibre)
+
 class Forma(models.Model):
     id = models.AutoField(primary_key = True)
     forma = models.CharField(max_length = 55, blank = True, null = True)
+
+    def __unicode__(self):
+        return self.forma
