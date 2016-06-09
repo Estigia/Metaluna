698d5ac471497c9217e287905e26de69f7558ef7
Carlos Garcia <carlosfipq@gmail.com>
2016-06-08 21:22:54 -0600

    terminada la aplicacion de localizaciones

--- src/Localizaciones/admin.py
+++ src/Localizaciones/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Departamento, Municipio
+# from .forms import MunicipioForm, DepartamentoForm
 # Register your models here.
+
+class AdminDepartamento(admin.ModelAdmin):
+    list_display = ["id","departamento"]
+    # form = DepartamentoForm
+
+class AdminMunicipio(admin.ModelAdmin):
+    list_display = ["id","municipio", "Departamento_id"]
+    # form = MunicipioForm
+    #* De esta manera se muestra el departamento al que pertenece el municipio
+
+admin.site.register(Departamento, AdminDepartamento)
+admin.site.register(Municipio, AdminMunicipio)
