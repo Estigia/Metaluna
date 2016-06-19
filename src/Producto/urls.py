90176b74751016eb3d10208ef0b2f2da2a85d6ae
Chofo2003 <chofo_03@hotmail.com>
2016-06-18 20:57:54 -0600

    Urls arregladas

--- src/Producto/urls.py
+++ src/Producto/urls.py
@@ -14,10 +14,10 @@ from .views import (
 
 urlpatterns = [
     url(r'^$', index, name = 'index'),
-    url(r'crear', producto, name = 'crear'),
+    url(r'crear$', producto, name = 'crear'),
     url(r'(?P<pk>\d+)',ProductoDetail.as_view(), name='detail'),
     url(r'crear_lote', lote, name = 'lote'),
-    url(r'tipo_producto', tipo_producto, name = 'tipo_producto'),
+    url(r'tipoproducto', tipo_producto, name = 'tipo_producto'),
     url(r'crear_forma', forma, name = 'forma'),
     url(r'crear_calibre', calibre, name = 'calibre'),
     url(r'crear_longitud', longitud, name = 'longitud'),
