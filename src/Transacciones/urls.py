7bcb544cc7b471ec9ed8cc78a77da608a8035a94
Carlos Garcia <carlosfipq@gmail.com>
2016-07-12 12:06:59 -0600

    agregado el index a transacciones

--- src/Transacciones/urls.py
+++ src/Transacciones/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from .views import ventas, compras, venta, compra
+from .views import ventas, compras, venta, compra, index
 
 urlpatterns = [
     url(r'^ventas/',ventas, name='ventas'),
     url(r'^compras/', compras, name='compras'),
     url(r'^venta/', venta, name='venta'),
-    url(r'^compra/', compra, name='compra')
+    url(r'^compra/', compra, name='compra'),
+    url(r'^$', index, name='index')
 ]
