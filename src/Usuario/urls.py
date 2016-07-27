f0aa67686ed704efa07028c4b21139496a41f209
MalvinJazz <malvinjazz@live.com>
2016-07-27 12:19:48 -0600

    Corregido template de lista.

--- src/Usuario/urls.py
+++ src/Usuario/urls.py
@@ -3,12 +3,13 @@ from django.conf.urls import url
 from .views import (
             EmpleadoCreate,
             empleadoList,
-            EmpleadoUpdate
+            EmpleadoUpdate,
+            EmpleadoDetail
             )
 
 urlpatterns = [
         url(r'^crear/', EmpleadoCreate.as_view(), name='empleadoCreate'),
         url(r'^$', empleadoList, name='empleadoList'),
         url(r'^(?P<pk>\d+)/editar', EmpleadoUpdate.as_view(), name='empleadoUpdate'),
-
+        url(r'^(?P<pk>\d+)/detalles', EmpleadoDetail.as_view(), name='empleadoDetail')
 ]
