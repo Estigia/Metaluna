0261cca08720951be36eaa50581a1ae085925e4a
Chofo2003 <chofo_03@hotmail.com>
2016-10-07 10:51:23 -0600

    Configurado Postgres

--- src/src/settings.py
+++ src/src/settings.py
@@ -82,9 +82,13 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+  'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'MetalunaDB',
+        'USER': 'metaluna',
+        'PASSWORD': 'SGeDU3yC',
+        'HOST': 'distribuidorametaluna.com',
+        'PORT': '5432',
     }
 }
 
