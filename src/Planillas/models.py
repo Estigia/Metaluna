6ea9764cf4e64765ce7f8729c01e6cb1cdb28106
Chofo2003 <chofo_03@hotmail.com>
2016-10-11 20:20:37 -0600

    Defaults en finanzas datetime

--- src/Planillas/models.py
+++ src/Planillas/models.py
@@ -26,7 +26,7 @@ class Planilla(models.Model):
 class Finanzas(models.Model):
     id = models.AutoField(primary_key = True)
     descripcion = models.CharField(max_length = 100, blank = False, null = False)
-    fecha = models.DateField()
+    fecha = models.DateField(auto_now_add=False, auto_now=True)
     #True = ingreso False = egreso
     tipo = models.BooleanField(default = None)
     monto = models.FloatField(blank = False, null = False)
