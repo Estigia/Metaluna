8fdd7340066c15b594141e92f357d563a1df2c62
23justo <justojul23@hotmail.com>
2016-12-14 12:07:27 -0600

    agregado plantillaLogin.html a views inicio de usuario

--- src/src/settings.py
+++ src/src/settings.py
@@ -83,12 +83,14 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
   'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'MetalunaDB',
-        'USER': 'metaluna',
-        'PASSWORD': 'SGeDU3yC',
-        'HOST': 'distribuidorametaluna.com',
-        'PORT': '5432',
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'MetalunaDB',
+        # 'USER': 'metaluna',
+        # 'PASSWORD': 'SGeDU3yC',
+        # 'HOST': 'distribuidorametaluna.com',
+        # 'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
